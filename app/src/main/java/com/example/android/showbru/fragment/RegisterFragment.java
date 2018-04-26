9b9da8949686bf62de87ec28c2a434d2d6c046c1
chalita <chalitapadtoom@gmail.com>
2018-04-26 17:01:47 +0700

    adapter OK

--- app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/android/showbru/fragment/RegisterFragment.java
@@ -1,16 +1,26 @@
 package com.example.android.showbru.fragment;
 
+import android.mtp.MtpConstants;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android.showbru.MainActivity;
 import com.example.android.showbru.R;
+import com.example.android.showbru.utility.AddNewUserToServer;
+import com.example.android.showbru.utility.MyAlert;
+import com.example.android.showbru.utility.MyConstant;
 
 public class RegisterFragment extends Fragment {
 
@@ -23,37 +33,101 @@ public class RegisterFragment extends Fragment {
 
     } // main Method
 
-    private void createToolbar() {
-        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
 
-//        Setup Title
-            ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
-            ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+            uploadValueToServer();
+            return true;
 
-//            Setup Navigation Icon
-                ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-                ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-                toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        getActivity().getSupportFragmentManager().popBackStack();
-                    }
-                });
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadValueToServer() {
+        //    get value from EditText
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passEditText = getView().findViewById(R.id.edtPassword);
+
+//        chang Data_Type from EditText to String
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passString = passEditText.getText().toString().trim();
+
+//        Check space
+        if (nameString.isEmpty() || userString.isEmpty() || passString.isEmpty()) {
+//            Have space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.normalDialog("Have Space","Please Fill All Blank");
+        }
+        else {
+//            No Space
+            try {
+
+                MyConstant myConstant = new MyConstant();
+                AddNewUserToServer addNewUserToServer = new AddNewUserToServer(getActivity());
+                addNewUserToServer.execute(nameString,userString,passString,
+                        myConstant.getUrlAddUser());
+
+                String result = addNewUserToServer.get();
+                Log.d("26AprilV1", "result ===>" + result);
+                if (Boolean.parseBoolean(result)) {
+                    getActivity().getSupportFragmentManager().popBackStack();
+
+                } else {
+
+                    Toast.makeText(getActivity(),"Error cannnot Upload",
+                            Toast.LENGTH_SHORT).show();
+
+
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
 
 
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+
+//            Setup Navigation Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+        setHasOptionsMenu(true);
 
 
+    }
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
 
     }
